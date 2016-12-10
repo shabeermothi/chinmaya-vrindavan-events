b8d9e84e51326364b115cb023c4a3784425e57d7
Shabeer Mothi <shabeermothi@theshabeer.com>
2016-12-09 21:47:26 -0500

    Added filter for active and expired events

--- public/js/ng-app/events/events.list.directive.js
+++ public/js/ng-app/events/events.list.directive.js
@@ -2,10 +2,11 @@
     'use strict';
 
     angular.module('events')
-        .directive('eventsList', EventsListDirective);
+        .directive('eventsList', EventsListDirective)
+        .filter('eventsFilter', EventsFilter);
 
     function EventsListDirective () {
-        EventsListDirectiveCtrl.$inject = ['$scope', '$log', 'EventsListService'];
+        EventsListDirectiveCtrl.$inject = ['$scope', '$log', 'EventsListService', '$filter'];
 
         return {
             restrict: 'A',
@@ -17,13 +18,50 @@
             controllerAs: 'eventsList'
         };
 
-        function EventsListDirectiveCtrl ($scope, $log, EventsListService) {
+        function EventsListDirectiveCtrl ($scope, $log, EventsListService, $filter) {
             var eventsList = this;
-            
-            EventsListService.getEvents().then(function (response) {
-                eventsList.events = response;
-            });
+
+            getAllEvents();
+
+            function getAllEvents () {
+                EventsListService.getEvents().then(function (response) {
+                    eventsList.events = response;
+                });
+            }
+
+            eventsList.filterEvents = function (eventType) {
+                EventsListService.getEvents().then(function (response) {
+                    eventsList.events = response;
+                    eventsList.events = $filter('eventsFilter')(eventsList.events, eventType);
+                });
+            };
         }
+    }
+    
+    function EventsFilter () {
+        return function (events, eventType) {
+            var filteredEvent, filteredEvents = [];
+
+            if (eventType === 'active') {
+                for (var x = 0; x < events.length; x++) {
+                    if (new Date(events[x].eventDate) > new Date()) {
+                        filteredEvent = events[x];
+                        filteredEvents.push(filteredEvent);
+                    }
+                }
+            } else if (eventType === 'expired') {
+                for (var x = 0; x < events.length; x++) {
+                    if (new Date(events[x].eventDate) < new Date()) {
+                        filteredEvent = events[x];
+                        filteredEvents.push(filteredEvent);
+                    }
+                }
+            } else {
+                filteredEvents = events;
+            }
+
+            return filteredEvents;
+        };
     }
 
 })();
